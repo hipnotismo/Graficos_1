09ebc603f6a6f08fb4073314c3d401439d0678c9
martin <martinconcetti97@gmail.com>
2022-10-19 05:39:49 -0300

    Renderer

--- Desarrollo_1_Engine/GraphicsEngine/Renderer.cpp
+++ Desarrollo_1_Engine/GraphicsEngine/Renderer.cpp
@@ -1,1 +1,109 @@
-#include "Renderer.h"
+#include "renderer.h"
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+Renderer::Renderer()
+{
+
+}
+
+void Renderer::Draw(float* vertex, unsigned int* index, glm::mat4 modelMatrix)
+{
+	UpdateUniformShaders(modelMatrix);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 18, vertex, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(float) * 3, index, GL_STATIC_DRAW);
+	glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+}
+
+void Renderer::CreateBuffers()
+{
+	glGenVertexArrays(1, &VAO); // first: Specifies the number of vertex array object
+								// second: the generated vertex array object names are stored
+	glGenBuffers(1, &VBO); //first: the number of buffer object
+	glGenBuffers(1, &EBO); //second: an array in which the generated buffer object names are stored
+}
+
+void Renderer::BindBuffers()
+{
+	glBindVertexArray(VAO);// Specifies the name of the vertex array to bind
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); //first: type buffer to bound
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); //second: the buffer
+}
+void Renderer::DefVertexAttribute()
+{
+	//first: defPosMemoryShader(Layout),second:countData, three: TypeDataAttribute, four: Normalize
+	//five: countDataForVertex * SizeOf(typeData), six:StartCount (void*)0) the first point in data to count
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0); //activate first: defPosMemoryShader(Layout)
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+}
+
+void Renderer::CallUniformShaders()
+{
+	model = glGetUniformLocation(program, "model");//search the model in the shader
+}
+
+void Renderer::UpdateUniformShaders(glm::mat4 modelMatrix)
+{
+	glUniformMatrix4fv(model, 1, GL_FALSE, glm::value_ptr(modelMatrix)); //update model in the shader
+}
+
+
+unsigned int Renderer::CompileShader(unsigned int type, const char* shaderPath) { //first: ShaderType(Fragment, vertex)
+																		//second:Dir to archive
+	unsigned int id = glCreateShader(type); // Create Shader
+
+	std::string sourceShaderCode; //store source archive
+
+	std::ifstream sourceShaderFile; // interact to archive
+
+	sourceShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+	try {
+		sourceShaderFile.open(shaderPath);
+		std::stringstream shaderStream;
+
+		shaderStream << sourceShaderFile.rdbuf(); //conversion to StreamString
+
+		sourceShaderFile.close();
+
+		sourceShaderCode = shaderStream.str(); //conversion to string
+	}
+	catch (std::ifstream::failure& e) {
+		std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+	}
+
+	const char* srcCode = sourceShaderCode.c_str(); // conversion to char
+
+	glShaderSource(id, 1, &srcCode, nullptr); //Set source to Shader
+											// First: Shader, Second: conunt elements in the string
+											// three: Specifies an array of pointers to strings containing the source
+											// four: Specifies an array of string lengths
+	glCompileShader(id); //Complie Shader
+
+	return id;
+}
+
+void Renderer::CreateProgram(const char* vertexShaderPath, const char* fragmentShaderPath)
+{
+	//first: vertex archive
+	//second: fragmentShader
+	program = glCreateProgram(); // create program
+	unsigned int vertex = CompileShader(GL_VERTEX_SHADER, vertexShaderPath);
+	unsigned int fragment = CompileShader(GL_FRAGMENT_SHADER, fragmentShaderPath);
+
+	glAttachShader(program, vertex); //attach with program
+	glAttachShader(program, fragment); //attach with program
+	glLinkProgram(program); // Link with OpenGL
+	glValidateProgram(program); //validate
+	glUseProgram(program);
+
+	glDetachShader(program, vertex); //UnAttach
+	glDetachShader(program, fragment);//UnAttach
+	glDeleteShader(vertex); //delete
+	glDeleteShader(fragment); //delete
+}
--- Desarrollo_1_Engine/GraphicsEngine/Renderer.h
+++ Desarrollo_1_Engine/GraphicsEngine/Renderer.h
@@ -1,5 +1,29 @@
-#pragma once
-class Renderer
-{
-};
+#ifndef RENDERER_H
+#define RENDERER_H
+#include "glm/glm.hpp"
+#include"glm/gtc/type_ptr.hpp"
 
+class  Renderer
+{
+private:
+	unsigned int model;
+public:
+	Renderer();
+	void Draw(float* vertex, unsigned int* index, glm::mat4 modelMatrix);
+	unsigned int VBO = 0; // VertexBufferObject
+	unsigned int VAO = 0; // VertexArrayObject
+	unsigned int EBO = 0; // ElementsBufferObject
+	unsigned int program = 0; // ProgramShader
+	void CreateBuffers();
+	void BindBuffers();
+	unsigned int CompileShader(unsigned int type, const char* shaderPath);
+	void CreateProgram(const char* vertexShaderPath, const char* pixelShaderPath);
+	void DefVertexAttribute();
+	void CallUniformShaders();
+	void UpdateUniformShaders(glm::mat4 modelMatrix);
+};
+// settings
+//const unsigned int SCR_WIDTH = 800;
+//const unsigned int SCR_HEIGHT = 600;
+
+#endif
