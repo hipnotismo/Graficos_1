7a8cfd24ef9b196118fcac9421b4eec30f0e9b93
martin <martinconcetti97@gmail.com>
2022-10-19 05:19:58 -0300

    Window class

--- Desarrollo_1_Engine/GraphicsEngine/Window.cpp
+++ Desarrollo_1_Engine/GraphicsEngine/Window.cpp
@@ -1,1 +1,43 @@
 #include "Window.h"
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+GLFWwindow* aux;
+
+Window::Window()
+{
+    window = aux;
+}
+
+Window::Window(int width, int height, const char* title, GLFWmonitor* monitor, GLFWwindow* share)
+{
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(width, height, title, monitor, share);
+}
+
+void Window::WindowContext()
+{
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+}
+
+bool Window::ShouldClose()
+{
+    return !glfwWindowShouldClose(window);
+}
+
+GLFWwindow* Window::GetWindow()
+{
+    return window;
+}
+void Window::ClearBackground()
+{
+    /* Start Render here */ //Clear the Background
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+void Window::SwapBuffer()
+{
+    /* Swap front and back buffers */
+    glfwSwapBuffers(window);
+}
--- Desarrollo_1_Engine/GraphicsEngine/Window.h
+++ Desarrollo_1_Engine/GraphicsEngine/Window.h
@@ -1,5 +1,19 @@
-#pragma once
+#ifndef WINDOW_H
+#define WINDOW_H
+
+struct GLFWmonitor;
+struct GLFWwindow;
 class Window
 {
+private:
+	GLFWwindow* window;
+public:
+	Window();
+	Window(int width, int height, const char* title, GLFWmonitor* monitor, GLFWwindow* share);
+	void WindowContext();
+	bool ShouldClose();
+	GLFWwindow* GetWindow();
+	void ClearBackground();
+	void SwapBuffer();
 };
-
+#endif
