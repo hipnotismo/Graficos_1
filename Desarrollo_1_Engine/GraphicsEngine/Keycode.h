acfe642fa2d1adb579f3f83fec00ff7dc325ca7f
martin <martinconcetti97@gmail.com>
2022-11-19 11:52:25 -0300

    Keycodes

---
+++ Desarrollo_1_Engine/GraphicsEngine/Keycode.h
@@ -0,0 +1,31 @@
+#ifndef KEYCODE_H
+#define KEYCODE_H
+
+#define KEYCODE_A                  65
+#define KEYCODE_B                  66
+#define KEYCODE_C                  67
+#define KEYCODE_D                  68
+#define KEYCODE_E                  69
+#define KEYCODE_F                  70
+#define KEYCODE_G                  71
+#define KEYCODE_H                  72
+#define KEYCODE_I                  73
+#define KEYCODE_J                  74
+#define KEYCODE_K                  75
+#define KEYCODE_L                  76
+#define KEYCODE_M                  77
+#define KEYCODE_N                  78
+#define KEYCODE_O                  79
+#define KEYCODE_P                  80
+#define KEYCODE_Q                  81
+#define KEYCODE_R                  82
+#define KEYCODE_S                  83
+#define KEYCODE_T                  84
+#define KEYCODE_U                  85
+#define KEYCODE_V                  86
+#define KEYCODE_W                  87
+#define KEYCODE_X                  88
+#define KEYCODE_Y                  89
+#define KEYCODE_Z                  90
+
+#endif;
